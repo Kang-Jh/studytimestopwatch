69a7ac2b5230002ef3dabd1cf35051e9e4a8ba0f
kang-jh <iamkangjh@gmail.com>
2020-08-19 17:42:30 +0900

    v0.16.0 remove tpyes and change a function name

--- src/functions/time.ts
+++ src/functions/time.ts
@@ -4,16 +4,13 @@ import { Time, TimeObject } from '../@types/time';
 export const timeObject: TimeObject =
   typeof performance === 'object' ? performance : Date;
 // getNow 함수를 이용하는 이유는 performance.now()를 사용할 수 없을 경우 Date.now()를 사용하기 위함
-export const getNow: () => number = () => timeObject.now();
-export const miliSecondsToSeconds: (miliseconds: number) => number = (
-  miliseconds: number
-): number => Math.floor(miliseconds / 1000);
+export const getNow = (): number => timeObject.now();
+export const convertMiliSecondsToSeconds = (miliseconds: number): number =>
+  Math.floor(miliseconds / 1000);
 
 // getTime는 초로 표현된 시간값을 입력값으로 받아
 // 몇 시간, 몇 분, 몇 초인지를 구할 수 있게 해줌
-export const convertSecondsToTime: (seconds: number) => Time = (
-  sec: number
-): Time => {
+export const convertSecondsToTime = (sec: number): Time => {
   const hours: number = Math.floor(sec / 3600);
   const minutes: number = Math.floor((sec - hours * 3600) / 60);
   const seconds: number = sec - hours * 3600 - minutes * 60;
@@ -25,7 +22,7 @@ export const convertSecondsToTime: (seconds: number) => Time = (
   };
 };
 
-export const convertTimeToSeconds: (time: Time) => number = (time) => {
+export const convertTimeToSeconds = (time: Time): number => {
   const hours = time.hours;
   const minutes = time.minutes;
   const seconds = time.seconds;
@@ -37,6 +34,6 @@ export const convertTimeToSeconds: (time: Time) => number = (time) => {
 // 입력값이 10 이상이면 입력값을 그대로 return
 // 이로써 화면에 표시되는 시(또는 분 또는 초)가
 // 10보다 작을 경우 앞에 0을 붙인 값이 화면에 표시됨
-export const getDisplayTime: (number: number) => string | number = (number) => {
-  return number < 10 ? `0${number}` : number;
+export const getDisplayTime = (number: number): string => {
+  return number < 10 ? `0${number}` : `${number}`;
 };
