f669b0116b670b6e787ab3fa7b088facca7060c7
kang-jh <iamkangjh@gmail.com>
2020-09-10 20:40:02 +0900

    add prop and change PeriodRecord type

--- src/@types/studyRecord.ts
+++ src/@types/studyRecord.ts
@@ -2,19 +2,20 @@ import { Time } from './time';
 
 export interface StudyRecord {
   id?: string;
+  localKey?: number;
   heading: string;
   date: string;
-  periodRecords: PeriodRecord[];
+  periodRecords: Partial<PeriodRecord>[];
   totalStudyTime: Time;
   totalRestTime: Time;
 }
 
 export interface PeriodRecord {
-  period?: number;
-  netStudyTimeHours?: number;
-  netStudyTimeMinutes?: number;
-  netStudyTimeSeconds?: number;
-  restTimeHours?: number;
-  restTimeMinutes?: number;
-  restTimeSeconds?: number;
+  period: number;
+  netStudyTimeHours: number;
+  netStudyTimeMinutes: number;
+  netStudyTimeSeconds: number;
+  restTimeHours: number;
+  restTimeMinutes: number;
+  restTimeSeconds: number;
 }
