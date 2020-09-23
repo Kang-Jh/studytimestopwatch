df7bf618d0d6d823e76399732dd6c00d14055087
kang-jh <iamkangjh@gmail.com>
2020-09-23 13:41:09 +0900

    change import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React, { useState, ReactElement } from 'react';
+import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import HeaderAndNav from './components/HeaderAndNav/HeaderAndNav';
+import { Header, Nav } from './components/HeaderAndNav/HeaderAndNav';
 import Stopwatch from './pages/stopwatch';
 import MyRecords from './pages/myRecords';
 import DetailRecord from './pages/detailRecord';
 import StatisticOfUsers from './pages/statisticOfUsers';
 import './App.css';
 
-function App(): ReactElement {
+function App() {
   const [isMenuOpened, setIsMenuOpened] = useState(false);
 
   const onMenuClicked = () => {
@@ -16,7 +16,8 @@ function App(): ReactElement {
 
   return (
     <div className="App" aria-live="assertive">
-      <HeaderAndNav isMenuOpened={isMenuOpened} onMenuClicked={onMenuClicked} />
+      <Header onMenuClicked={onMenuClicked} />
+      <Nav isMenuOpened={isMenuOpened} onMenuClicked={onMenuClicked} />
       <main
         onClick={() => {
           if (window.innerWidth < 600) {
