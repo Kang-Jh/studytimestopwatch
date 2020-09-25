0be731596027b4c5124922a0732d1a11738e9f22
kang-jh <iamkangjh@gmail.com>
2020-09-25 10:35:30 +0900

    add prop to handle to showing menu button

--- src/components/HeaderAndNav/HeaderAndNav.tsx
+++ src/components/HeaderAndNav/HeaderAndNav.tsx
@@ -5,60 +5,46 @@ import './Nav.css';
 import menuIcon from './images/menu-icon.svg';
 import { OnClick } from '../../@types/AppEvent';
 
-export function Header({ onMenuClicked }: { onMenuClicked: OnClick }) {
+export function Header({
+  onMenuClicked,
+  showMenuButton,
+}: {
+  onMenuClicked: OnClick;
+  showMenuButton: boolean;
+}) {
   return (
     <header role="banner" className="Header primary">
       <h1 className="srOnly">공부용 스톱워치 애플리케이션</h1>
-      <div className="Header-menuDiv">
+      <div className={`${showMenuButton ? 'Header-menuDiv' : 'srOnly'}`}>
         <button className="button primary" onClick={onMenuClicked}>
-          <img
-            className="Header-menuImage"
-            src={menuIcon}
-            alt="메뉴 열기 버튼"
-          />
+          <img className="Header-menuImage" src={menuIcon} alt="메뉴 버튼" />
         </button>
       </div>
     </header>
   );
 }
 
-export function Nav({
-  isMenuOpened,
-  onMenuClicked,
-}: {
-  isMenuOpened: boolean;
-  onMenuClicked: OnClick;
-}) {
+export function Nav({ isMenuOpened }: { isMenuOpened: boolean }) {
   return (
-    <div className={`NavWrapper ${isMenuOpened ? '' : 'srOnly'}`}>
-      <div className="Nav-menuDivCover primary">
-        <div className="Nav-dummyDiv">
-          <button className="button primary" onClick={onMenuClicked}>
-            <img src={menuIcon} alt="메뉴 닫기 버튼" />
-          </button>
-        </div>
-      </div>
+    <nav role="navigation" className={`Nav ${isMenuOpened ? '' : 'srOnly'}`}>
+      <ul>
+        <li>
+          <Link to="/" className="Nav-link">
+            스톱워치
+          </Link>
+        </li>
+        <li>
+          <Link to="/myRecords" className="Nav-link">
+            내 기록
+          </Link>
+        </li>
 
-      <nav role="navigation" className="Nav">
-        <ul>
-          <li>
-            <Link to="/" className="Nav-link">
-              스톱워치
-            </Link>
-          </li>
-          <li>
-            <Link to="/myRecords" className="Nav-link">
-              내 기록
-            </Link>
-          </li>
-
-          <li>
-            <Link to="/statisticOfUsers" className="Nav-link">
-              전체유저 통계
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+        <li>
+          <Link to="/statisticOfUsers" className="Nav-link">
+            전체유저 통계
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
