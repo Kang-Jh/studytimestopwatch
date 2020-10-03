6b67d2336500ad3968c1c42a5af0af949892b983
kang-jh <iamkangjh@gmail.com>
2020-10-03 20:47:14 +0900

    apply material-ui

--- src/pages/MyRecords.tsx
+++ src/pages/MyRecords.tsx
@@ -7,14 +7,42 @@ import {
   convertTimeAsKorean,
   getDateAsKorean,
 } from '../utils/time';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 import Statistic from '../components/Statistic';
+import {
+  Button,
+  makeStyles,
+  Grid,
+  List,
+  ListItem,
+  ListItemText,
+  Link,
+  Collapse,
+  Typography,
+} from '@material-ui/core';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles((theme) => ({
+  flex: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  columnDirection: {
+    flexDirection: 'column',
+  },
+  textRight: {
+    textAlign: 'right',
+  },
+}));
 
 export default function (props: any) {
+  const classes = useStyles();
   const [records, setRecords] = useState<StudyRecord[]>([]);
   const dates = [
     ...new Set(records.map((record) => getDateAsKorean(record.date))),
   ].sort();
+  const [openRecord, setOpenRecord] = useState('');
   const totalPeriod: number = (records as any[]).reduce(
     (prevTotalPeriod: number, record: StudyRecord): number => {
       return prevTotalPeriod + record.periodRecords.length;
@@ -59,53 +87,88 @@ export default function (props: any) {
   }, []);
 
   return (
-    <div className="MyRecords">
-      <div>
-        <button
+    <Grid container justify="center" alignItems="center">
+      <Grid item xs={12} md={7} className={classes.textRight}>
+        <Button
+          size="large"
+          variant="contained"
+          color="secondary"
           onClick={() => {
             localStorage.clear();
             setRecords([]);
           }}
         >
           리셋
-        </button>
-      </div>
-      <Statistic
-        heading="전체 통계"
-        totalPeriod={totalPeriod}
-        totalStudyTime={totalStudyTime}
-        totalRestTime={totalRestTime}
-      />
+        </Button>
+      </Grid>
 
-      <div>
-        <h2>공부 기록 목록</h2>
-        <ol>
+      <Grid item xs={12} md={7}>
+        <Statistic
+          heading="전체 통계"
+          totalPeriod={totalPeriod}
+          totalStudyTime={totalStudyTime}
+          totalRestTime={totalRestTime}
+        />
+      </Grid>
+
+      <Grid item xs={12} md={7}>
+        <Typography variant="srOnly" component="h2">
+          공부 기록 목록
+        </Typography>
+        <List component="ol">
           {/* 날짜를 기준으로 기록을 분류 */}
           {dates.map((date) => (
-            <li key={date}>
-              <span>{date}</span>
-              <ul>
-                {/* 기록에서 같은 날짜인 것들만 필터링 해서 렌더링 */}
-                {records
-                  .filter((record) => date === getDateAsKorean(record.date))
-                  .map((record) => (
-                    <li key={(record.localKey as number).toString()}>
-                      <h3>{record.heading}</h3>
-                      <div>{getDateAsKorean(record.date)}</div>
-                      <div>
-                        공부시간 {convertTimeAsKorean(record.totalStudyTime)}
-                      </div>
-                      <div>
-                        휴식시간 {convertTimeAsKorean(record.totalRestTime)}
-                      </div>
-                      <Link to={`/myRecords/${record.id}`}>세부 기록</Link>
-                    </li>
-                  ))}
-              </ul>
-            </li>
+            <ListItem
+              className={classes.columnDirection}
+              component="li"
+              button
+              key={date}
+              onClick={() => {
+                if (openRecord === date) {
+                  setOpenRecord('');
+                } else {
+                  setOpenRecord(date);
+                }
+              }}
+            >
+              <div className={classes.flex}>
+                <ListItemText>{date}</ListItemText>
+                {openRecord === date ? <ExpandLess /> : <ExpandMore />}
+              </div>
+
+              <Collapse component="div" in={openRecord === date} timeout="auto">
+                <List>
+                  {/* 기록에서 같은 날짜인 것들만 필터링 해서 렌더링 */}
+                  {records
+                    .filter((record) => date === getDateAsKorean(record.date))
+                    .map((record) => (
+                      <ListItem
+                        className={classes.columnDirection}
+                        key={(record.localKey as number).toString()}
+                      >
+                        <ListItemText>{record.heading}</ListItemText>
+                        <ListItemText>
+                          공부시간 {convertTimeAsKorean(record.totalStudyTime)}
+                        </ListItemText>
+                        <ListItemText>
+                          휴식시간 {convertTimeAsKorean(record.totalRestTime)}
+                        </ListItemText>
+                        <ListItemText>
+                          <Link
+                            component={RouterLink}
+                            to={`/myRecords/${record.id}`}
+                          >
+                            세부 기록
+                          </Link>
+                        </ListItemText>
+                      </ListItem>
+                    ))}
+                </List>
+              </Collapse>
+            </ListItem>
           ))}
-        </ol>
-      </div>
-    </div>
+        </List>
+      </Grid>
+    </Grid>
   );
 }
